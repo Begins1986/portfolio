b25b16ff443de2cc2a9b8fda660fdb1ace60769a
Begin Sergey <begins.1986@mail.ru>
2022-04-06 23:06:51 +0300

    Add first block

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import s from './Main.module.css'
+import st from './../common/style/Container.module.css'
 
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-
+            <div className={st.container}>
+                <div className={s.text}>
+                    <span>Hi there</span>
+                    <h1>I am Begin Sergey</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
+            </div>
         </div>
     );
 };
